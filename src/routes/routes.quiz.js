7f58352a17a5be8061a57087e45d34bba4bac563
jrlopes <contacto.juniorlope@gmail.com>
2024-03-13 15:22:00 -0500

    agregando opcion de consultas encadenadas

--- src/routes/routes.quiz.js
+++ src/routes/routes.quiz.js
@@ -1,24 +1,30 @@
-import {Router} from 'express'
-import fs from 'fs'
-import path from 'path'
+import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
 
-const quizFile = path.join(process.cwd(), "src/db", "data.json")
-const router = Router()
+const quizFile = path.join(process.cwd(), "src/db", "data.json");
+const router = Router();
 
 router.get('/', (req, res) => {
-    const quizzes = readFile()
-    res.json(quizzes)
- })
+    const { category, level } = req.query;
+    const quizzes = readFile();
+    let filteredQuizzes = quizzes;
 
+    if (category && level) {
+        // Filtrar los quizzes basados en los parámetros de categoría y nivel
+        filteredQuizzes = quizzes.filter(quiz => quiz.category === category && quiz.level === level);
+    }
+
+    res.json(filteredQuizzes);
+});
 
 function readFile() {
-    const result = fs.readFileSync(quizFile, "utf-8")
-    const json = JSON.parse(result)
-    return json
+    const result = fs.readFileSync(quizFile, "utf-8");
+    const json = JSON.parse(result);
+    return json;
 }
 
+export default router;
 
-
- export default router
 
 
